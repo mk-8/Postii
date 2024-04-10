25ec0de8de3efe5df9d60f4dfe4e73771015a1e0
Meet K <62850330+mk-8@users.noreply.github.com>
2024-04-10 17:12:37 +0530

    added like and comm icon on profile-page

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -1,10 +1,17 @@
- <a href="/post/{{$post->id}}" class="list-group-item list-group-item-action">
-    <img class="avatar-tiny" src="{{$post->user->avatar}}" />
-    <strong>{{$post->title}}</strong> 
-    <span class= "text-muted"> 
-        @if (!isset($hideAuthor))
-        by {{$post->user->username}}
-        @endif
-        on {{$post->created_at->format('j/n/Y')}} 
-    </span>
+<a href="/post/{{$post->id}}" class="list-group-item list-group-item-action" style="display: flex; justify-content: space-between; align-items: center;">
+    <div>
+        <img class="avatar-tiny" src="{{$post->user->avatar}}" />
+        <strong>{{$post->title}}</strong> 
+        <span class="text-muted"> 
+            @if (!isset($hideAuthor))
+                by {{$post->user->username}}
+            @endif
+            on {{$post->created_at->format('j/n/Y')}}
+        </span>
+    </div>
+    <span style="margin-right: 1.5rem">
+        <i class="fa fa-heart" aria-hidden="true"></i> {{ $post->likes()->count() }}
+        &nbsp;&nbsp;
+        <i class="fa fa-comment" aria-hidden="true"></i> {{ $post->comments()->count() }}
+    </span>    
 </a>
